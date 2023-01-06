02e16ad02188ab0bd3babbed31c26a76836a3214
Andreas Jansson <andreas.jansson234@gmail.com>
2023-01-06 19:02:18 +0100

    Add my_select method to 'Enumerable' module
    my_select will iterate over elements and return elements
    that return true to condition in given block.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,12 +7,16 @@ module Enumerable
       index += 1
     end
   end
+
+  def my_select
+    arr = []
+    my_each do |element|
+      arr << element if yield(element)
+    end
+    arr
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   def my_each()
     for element in self
